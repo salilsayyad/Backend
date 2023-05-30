f9e758d27546536fdb94a0e931c55606e2ba6d32
Salil <1999salil2016@gmail.com>
2023-05-30 14:05:42 +0530

    f

--- Server/database/db.js
+++ Server/database/db.js
@@ -7,7 +7,7 @@ const DBConnection = async () => {
     const USERNAME = process.env.DB_USERNAME;
     const PASSWORD = process.env.DB_PASSWORD;
 
-    const MONGO_URI = "mongodb+srv://user:Delta@123456789@cluster0.wwxcdie.mongodb.net/?retryWrites=true&w=majority";
+    const MONGO_URI = "mongodb+srv://user:salil@cluster0.wwxcdie.mongodb.net/?retryWrites=true&w=majority";
     try {
         await mongoose.connect("mongodb+srv://user:salil@cluster0.wwxcdie.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true });
         console.log('Database connected successfully');
